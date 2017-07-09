3379f42a7124173cae9c684b1333766dcd3a9072
Mahendran Kannan <mahendrankannan@yahoo.com>
2017-07-09 22:56:01 +0530

    Basic node project setup done

---
+++ index.js
@@ -0,0 +1,28 @@
+// require dependencies
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+// express app
+const app = express()
+
+const port = 3000
+
+// add rendering engine and setup directories
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+// routes
+app.get('/', (request, response) => {
+    response.render('login')
+})
+
+app.listen(port)
+
+// To debug
+//DEBUG=express* node index.js
