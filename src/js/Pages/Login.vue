0de1b882ebc5783940f26b509e70cf253eee8861
Mahendran Kannan <mahendrankannan@yahoo.com>
2017-07-11 01:30:43 +0530

    Added Login Form - add form styles and updated grids styles

--- src/js/Pages/Login.vue
+++ src/js/Pages/Login.vue
@@ -7,6 +7,19 @@
                 <div class="col-12">
                     <p class="lead">Register with us today to receive regular updates from the HelloFresh farm. Inspiring recipe ideas, the newest food trends and unique HelloFresh promotions are awaiting you. And, don't worry, we promise not to spam you with daily emails</p>
                 </div>
+                <div class="col-6 col-offset-3">
+                    <form method="post" class="form form--vertical">
+                        <div class="form__element">
+                            <input type="text" placeholder="Email">
+                        </div>
+                        <div class="form__element">
+                            <input type="password" placeholder="Password">
+                        </div>
+                        <div class="form__element">
+                            <input type="submit" class="btn btn--primary" value="Login">
+                        </div>
+                    </form>
+                </div>
             </div>
         </section>
 
