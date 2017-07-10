7fd33cd8a9d2b38169fce8d2abeb086325cfc873
Mahendran Kannan <mahendrankannan@yahoo.com>
2017-07-11 00:25:39 +0530

    configured express for SPA and added Vue dependencies

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
 // require dependencies
-const path = require('path')
 const express = require('express')
-const exphbs = require('express-handlebars')
+const history = require('connect-history-api-fallback');
+
+// port
+const port = 3000
 
 // express app
 const app = express()
 
-const port = 3000
-
-// add rendering engine and setup directories
-app.engine('.hbs', exphbs({
-    defaultLayout: 'main',
-    extname: '.hbs',
-    layoutsDir: path.join(__dirname, 'views/layouts')
-}))
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'views'))
-
 // setup static path
 app.use(express.static('dist'))
 
-// routes
-app.get('/', (request, response) => {
-    response.render('login')
-})
+// Use history api fallback
+app.use(history());
 
+// listen on port
 app.listen(port)
-
-// To debug
-//DEBUG=express* node index.js
